94f3a49c376503ed644135fb10a3205952ed766d
Aiperi Zhenishova <zhenishovaaiperi@gmail.com>
2024-10-17 22:27:02 +0600

    fixed bugs

--- src/main/java/kg/alatoo/taskmanagementsystem/entities/TaskEntity.java
+++ src/main/java/kg/alatoo/taskmanagementsystem/entities/TaskEntity.java
@@ -6,10 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-<<<<<<< HEAD
-=======
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
->>>>>>> 32087394a541ad60dc2bd793e134aa9d5b48532f
+
 
 import java.time.LocalDateTime;
 
@@ -32,9 +30,6 @@ public class TaskEntity {
     private TaskStatus status;
 
     @ManyToOne
-<<<<<<< HEAD
     private WorkerEntity user;
-=======
-    private UserEntity user;
->>>>>>> 32087394a541ad60dc2bd793e134aa9d5b48532f
+
 }
